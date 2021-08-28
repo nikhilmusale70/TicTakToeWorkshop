7c59d21c8829ad80bc42d18e7494352700aea7e9
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-28 11:43:54 +0530

    [Nikhil] - Added user symbol and comp symbol

--- src/com/bridgelabz/tictaktoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictaktoe/TicTacToeGame.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.tictaktoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
-    public void board(){
+    public char[] createBoard(){
         char[] board = new char[10];
         for (int i=1;i<=10;i++)
             board[i] = ' ';
+        return board;
+    }
+
+    public char chooseSymbol(){
+        System.out.println("Which symbol you want \n'x' or 'o'");
+        Scanner sc = new Scanner(System.in);
+        char userSymbol = sc.next().charAt(0);
+        return userSymbol;
     }
 }
--- src/com/bridgelabz/tictaktoe/TicTacToeRunner.java
+++ src/com/bridgelabz/tictaktoe/TicTacToeRunner.java
@@ -1,8 +1,16 @@
 package com.bridgelabz.tictaktoe;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
 public class TicTacToeRunner {
     public static void main(String[] args) {
         TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        ticTacToeGame.board();
+        char[] board = ticTacToeGame.createBoard();
+        char userSymbol = ticTacToeGame.chooseSymbol();
+        char computerSymbol;
+        if (userSymbol=='x')
+            computerSymbol = 'o';
+        else
+            computerSymbol = 'x';
     }
 }
