e0bb6b5cb358cdf14640a1c7cda36bc3e2ab2c48
Nikhil Musale <nikhilmusale70@gmail.com>
2021-08-28 11:22:16 +0530

    [Nikhil] - added board structure

--- src/com/bridgelabz/tictaktoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictaktoe/TicTacToeGame.java
@@ -3,7 +3,7 @@ package com.bridgelabz.tictaktoe;
 public class TicTacToeGame {
     public void board(){
         char[] board = new char[10];
-        for (int i=0;i<10;i++)
+        for (int i=1;i<=10;i++)
             board[i] = ' ';
     }
 }
